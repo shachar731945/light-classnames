a49cf78158c55d5c2a4c3fbf5cbf9638816d8bb1
shahar avni <shachar731945@gmail.com>
2021-09-18 22:47:25 +0300

    fixed import warnings

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,10 @@ import typescript from '@rollup/plugin-typescript';
 export default {
     input: 'src/index.ts',
     output: {
+        sourcemap: true,
         dir: 'dist',
-        format: 'cjs'
+        format: 'cjs',
+        exports: 'auto'
     },
     plugins: [typescript()]
 }
