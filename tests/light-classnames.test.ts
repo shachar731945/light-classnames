f559d13b183a04310b850832d0590e0acdc7226a
shahar avni <shachar731945@gmail.com>
2021-09-21 11:58:14 +0300

    changed naming for testCases types

--- tests/light-classnames.test.ts
+++ tests/light-classnames.test.ts
@@ -1,7 +1,7 @@
 import lightClassNamesTestCases from "./testCases";
 import lightClassNames from '../src/index';
 
-lightClassNamesTestCases.forEach(testCase => {
+lightClassNamesTestCases.forEach((testCase) => {
     const {name, value, expectedResult} = testCase;
     test(name, () => {
         expect(lightClassNames(...value)).toEqual(expectedResult);
