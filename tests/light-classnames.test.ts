c30487f1cdc269cb376be8cad52c55516b9fd9fd
Yehuda Goldberg <hvusvd@gmail.com>
2021-10-20 20:59:35 +0300

    lightClassNamesTestCases as test in test suite

--- tests/light-classnames.test.ts
+++ tests/light-classnames.test.ts
@@ -1,9 +1,10 @@
 import lightClassNamesTestCases from "./testCases";
 import lightClassNames from '../src/index';
 
-test('check the function parses correctly', () => {
+describe('Check the function parses correctly', () => {
     lightClassNamesTestCases.forEach(testCase => {
-        expect(lightClassNames(...testCase.value))
-        .toEqual(testCase.expectedResult);
-    })
+        test(testCase.name, () => {
+            expect(lightClassNames(...testCase.value)).toEqual(testCase.expectedResult);
+        });
+    });
 });
