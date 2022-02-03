762d029bfb63017cf175bce9398a8716b79853f2
Yehuda Goldberg <hvusvd@gmail.com>
2022-02-03 13:47:51 +0200

    config rollup bundle (#2)
    * clean ignore fiels
    
    * npm up
    
    * readmy
    
    * rollup cjs and ts config
    
    * clean externals
    
    * clean and prepack scripts
    
    * fix tests ignor
    
    * added transpile with sourcemap for development
    
    * clean
    
    * npm up

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,22 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
+import pkg from './package.json';
+
+const externals = []
+    .concat(Object.keys(pkg.dependencies || {}))
+    .concat(Object.keys(pkg.peerDependencies || {}));
+
+const generateConfig = format => ({
+    external: path => externals.some(external => path.startsWith(external)),
     input: 'src/index.ts',
     output: {
-        sourcemap: true,
-        dir: 'dist',
-        format: 'cjs',
-        exports: 'auto'
+        exports: 'auto',
+        file: `dist/bundle.${format}.js`,
+        format
     },
-    plugins: [typescript()]
-}
+    plugins: [
+        typescript({tsconfig: './tsconfig.json'})
+    ]
+});
+
+export default ['esm', 'cjs'].map(generateConfig);
