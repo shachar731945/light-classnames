cf1bf880b10e0a455e0b33090f0e4daa0a564516
Yehuda Goldberg <hvusvd@gmail.com>
2022-02-18 15:53:33 +0200

    update comment

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 export type TruthyArgumentType = string | object | number;
 export type ArgumentType = TruthyArgumentType | null | undefined;
 
-// using `arguments` instead normal spread operator (...) due to performance reasons.
+// Using `arguments` instead normal spread operator (...) due to performance reasons.
 /** Calculates the classNames based on series of arguments */
 const lightClassNames: (...args: ArgumentType[]) => string = function () {
     let classNames = '', index = 0, argumentsLength = arguments.length, key;
 
-    // using while instead of for-loop for better performance.
+    // Using while instead of for-loop for better performance.
     while (index < argumentsLength) {
         const arg: ArgumentType = arguments[index];
         if (arg) {
             if (typeof arg === 'object') {
-                // iterating over object with for-in. In order to improve performance.
+                // Iterating over object with for-in. This is in order to improve performance.
                 for (key in arg) {
                     if (arg[key as keyof typeof arg]) {
                         if (classNames) classNames += ' ';
