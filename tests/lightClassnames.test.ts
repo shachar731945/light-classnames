4261ca375ebd6a5cd35bef22445646089380a518
shahar avni <shachar731945@gmail.com>
2022-02-26 21:20:32 +0200

    changed import to pass tests

--- tests/lightClassnames.test.ts
+++ tests/lightClassnames.test.ts
@@ -1,5 +1,5 @@
 import {testCases} from "./testCases";
-import lightClassNames from '../src';
+import lightClassNames from '../src/index';
 
 describe('Check the function parses correctly', () => {
     testCases.forEach(testCase => {
